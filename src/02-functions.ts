66658d4a24dd92d68ddf7e555f9848d662a2805c
JasonLiu <jasonliudehui227@gmail.com>
2026-09-15 10:33:37 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,5 +1,5 @@
 import {Friend, Colleague } from './myTypes'
-import { friends } from './01-basics'
+import { friends, colleagues } from './01-basics'
 
 function older(f: Friend) : string {
      f.age += 1
@@ -7,3 +7,29 @@ function older(f: Friend) : string {
 }
 
 console.log(older(friends[0]))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
+}
+console.log(highestExtension(colleagues.current));
+
+
+function addColleague(cs: Colleague[], name: string, department: string, email: string){
+  const highestExt = highestExtension(cs);
+  const newColleague: Colleague = {
+    name,
+    department,
+    contact: {
+      email,
+      extension: highestExt.contact.extension + 1,
+    },
+  };
+  cs.push(newColleague);
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
