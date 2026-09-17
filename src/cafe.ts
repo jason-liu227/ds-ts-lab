a5bdde21020669293111673ce989da202f76c013
JasonLiu <jasonliudehui227@gmail.com>
2026-09-17 14:26:37 +0100

    change lunch combo to soup for available describe

--- src/cafe.ts
+++ src/cafe.ts
@@ -161,6 +161,6 @@ console.log(allergyCard(brownie));
 
 // TS: Three more lines below are bugs that only the compiler can see. Once
 //     your types are in place, fix each one and note it in your commit message.
-console.log(describe(lunchCombo));
+console.log(describe(soup));
 console.log(updateItem(soup, { price: "7.00" }));
 console.log(firstMatch(menu, (i) => i.calories < 300));
